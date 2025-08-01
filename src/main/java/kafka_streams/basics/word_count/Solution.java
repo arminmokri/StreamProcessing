39385941695496097cc68eb14a99404524b6d198
Armin Mokri <arminmokri@gmail.com>
2025-08-01 09:50:50 -0400

    add Basics -> Word Count

---
+++ src/main/java/kafka_streams/basics/word_count/Solution.java
@@ -0,0 +1,131 @@
+package kafka_streams.basics.word_count;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Solution {
+
+    private static final String APPLICATION_ID = "word-count" + UUID.randomUUID();
+    private static final String CLIENT_ID = "word-count-client";
+    public static final String BOOTSTRAP_SERVERS = "localhost:9092";
+
+    private static Path STATE_DIR;
+
+    private KafkaStreams streams;
+
+    public StreamsBuilder buildWordCountTopology(String inputTopic, String outputTopic) {
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> stream = builder.stream(inputTopic);
+
+        KTable<String, Long> counts = stream
+                .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+                .groupBy((key, word) -> word)
+                .count();
+
+        counts.toStream().to(outputTopic);
+        return builder;
+    }
+
+    public void startStream(String inputTopic, String outputTopic) {
+        StreamsBuilder builder = buildWordCountTopology(inputTopic, outputTopic);
+
+        try {
+            STATE_DIR = Files.createTempDirectory(APPLICATION_ID).toAbsolutePath();
+        } catch (IOException ioException) {
+        }
+
+        streams = new KafkaStreams(builder.build(), getStreamsConfiguration());
+        streams.start();
+
+        // Wait briefly to ensure the topology is ready
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void stopStream() {
+        if (streams != null) {
+            streams.close();
+        }
+
+        if (Objects.nonNull(STATE_DIR)) {
+            try {
+                Files.walk(STATE_DIR)
+                        .sorted(Comparator.reverseOrder())
+                        .forEach(file -> {
+                                    try {
+                                        Files.delete(file);
+                                    } catch (IOException ioException) {
+                                    }
+                                }
+                        );
+            } catch (IOException ioException) {
+
+            }
+
+        }
+    }
+
+    private static Properties getStreamsConfiguration() {
+
+        Properties props = new Properties();
+
+        // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
+        // against which the application is run.
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, CLIENT_ID);
+
+        // Where to find Kafka broker(s).
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+
+        // Specify default (de)serializers for record keys and for record values.
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        // Records should be flushed every 100 ms. This is less than the default
+        // in order to keep this example interactive.
+        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 200); // faster commit for testing
+
+        // For illustrative purposes we disable record caches.
+        //props.put(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, 0);
+
+        //props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE_V2);
+
+        // Use a temporary directory for storing state, which will be automatically removed after the test.
+        props.put(StreamsConfig.STATE_DIR_CONFIG, STATE_DIR.toString());
+
+        return props;
+    }
+
+    public static void createTopic(String topic) {
+        try (AdminClient admin = AdminClient.create(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS))) {
+            admin.createTopics(List.of(new NewTopic(topic, 1, (short) 1))).all().get();
+        } catch (Exception exception) {
+
+        }
+    }
+
+    public static void deleteTopic(String topic) {
+        try (AdminClient admin = AdminClient.create(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS))) {
+            admin.deleteTopics(List.of(topic)).all().get();
+        } catch (Exception exception) {
+
+        }
+    }
+}
+
