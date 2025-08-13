87c28621527d723f6536f33dfb1f3ff3739243ac
Armin Mokri <arminmokri@gmail.com>
2025-08-13 09:09:20 -0400

    Custom Aggregator

---
+++ src/main/java/kafka_streams/aggregation/custom_aggregator/Solution.java
@@ -0,0 +1,155 @@
+package kafka_streams.aggregation.custom_aggregator;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Solution {
+
+    private static final String APPLICATION_ID = "custom_aggregator" + "_" + UUID.randomUUID();
+    private static final String CLIENT_ID = "custom_aggregator" + "_client";
+    public static final String BOOTSTRAP_SERVERS = "localhost:9092";
+
+    private static Path STATE_DIR;
+
+    private KafkaStreams streams;
+
+    private Topology buildTopology(String inputTopic, String outputTopic) {
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        Consumed<String, String> consumed = Consumed.with(Serdes.String(), Serdes.String());
+        Produced<String, String> produced = Produced.with(Serdes.String(), Serdes.String());
+        Grouped<String, String> grouped = Grouped.with(Serdes.String(), Serdes.String());
+        Materialized<String, String, KeyValueStore<Bytes, byte[]>> materializedStore =
+                Materialized
+                        .<String, String, KeyValueStore<Bytes, byte[]>>as("custom_aggregator-store")
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(Serdes.String());
+
+        KStream<String, String> kStream = builder.stream(inputTopic, consumed);
+
+        KTable<String, String> kTableSum = kStream
+                .peek((key, value) -> System.out.println("input from topic -> key='" + key + "' value='" + value + "'"))
+                .groupByKey(grouped)
+                .aggregate(
+                        () -> "",
+                        ((key, value, aggregate) -> {
+                            if (aggregate.isEmpty()) {
+                                return value;
+                            } else {
+                                return aggregate + "-" + value;
+                            }
+                        }),
+                        materializedStore
+                );
+
+        kTableSum.toStream()
+                .peek((key, value) -> System.out.println("output to topic -> key='" + key + "' value='" + value + "'"))
+                .to(outputTopic, produced);
+
+        return builder.build();
+    }
+
+    public void startStream(String inputTopic, String outputTopic) {
+
+        Topology topology = buildTopology(inputTopic, outputTopic);
+
+        try {
+            STATE_DIR = Files.createTempDirectory(APPLICATION_ID).toAbsolutePath();
+        } catch (IOException ioException) {
+        }
+
+        streams = new KafkaStreams(topology, getStreamsConfiguration());
+        streams.start();
+
+        // Wait briefly to ensure the topology is ready
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void stopStream() {
+        if (streams != null) {
+            streams.close();
+        }
+
+        if (Objects.nonNull(STATE_DIR)) {
+            try {
+                Files.walk(STATE_DIR)
+                        .sorted(Comparator.reverseOrder())
+                        .forEach(file -> {
+                                    try {
+                                        Files.delete(file);
+                                    } catch (IOException ioException) {
+                                    }
+                                }
+                        );
+            } catch (IOException ioException) {
+
+            }
+
+        }
+    }
+
+    private static Properties getStreamsConfiguration() {
+
+        Properties props = new Properties();
+
+        // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
+        // against which the application is run.
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, CLIENT_ID);
+
+        // Where to find Kafka broker(s).
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+
+        // Specify default (de)serializers for record keys and for record values.
+        //props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        //props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        // Records should be flushed every 100 ms. This is less than the default
+        // in order to keep this example interactive.
+        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 200); // faster commit for testing
+
+        // For illustrative purposes we disable record caches.
+        //props.put(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, 0);
+
+        // Use a temporary directory for storing state, which will be automatically removed after the test.
+        props.put(StreamsConfig.STATE_DIR_CONFIG, STATE_DIR.toString());
+
+        return props;
+    }
+
+    public static void createTopic(String topic) {
+        try (AdminClient admin = AdminClient.create(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS))) {
+            admin.createTopics(List.of(new NewTopic(topic, 1, (short) 1))).all().get();
+        } catch (Exception exception) {
+
+        }
+    }
+
+    public static void deleteTopic(String topic) {
+        try (AdminClient admin = AdminClient.create(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS))) {
+            admin.deleteTopics(List.of(topic)).all().get();
+        } catch (Exception exception) {
+
+        }
+    }
+}
+
